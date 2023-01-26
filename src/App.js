bd635aff9d5da8647b3e62643b0157b976911604
ljk9410 <dkssud6115@gmail.com>
2023-01-26 16:44:13 +0900

    pull

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <p>
-        안녕하세요 한준호 작업물 입니다
-      </p>
+      <p>안녕하세요 한준호 작업물 입니다</p>
       <ol>
         <li>1</li>
         <li>2</li>
